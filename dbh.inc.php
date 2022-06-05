18a3a49f9ecd50497a08a9dd5bad74b273cc8b26
theaanderschou <74912677+theaanderschou@users.noreply.github.com>
2022-06-05 20:38:56 +0200

    Update dbh.inc.php

--- dbh.inc.php
+++ dbh.inc.php
@@ -1,15 +1,14 @@
 <?php
-/*
+
 $dbServername = "theaanderschou.com.mysql";
 $dbUsername = "theaanderschou_comwordpress";
 $dbPassword = "#BabyBias";
 $dbName = "theaanderschou_comwordpress";
-*/
 
-$dbServername = "localhost";
+/*$dbServername = "localhost";
 $dbUsername = "root";
 $dbPassword = "root";
 $dbName = "morningcatch-db";
-
+*/
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
